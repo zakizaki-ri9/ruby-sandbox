34c353b2d483e45117bc67b05b2e97650cd7d93d
zakizaki-ri9 <kic.yuuki@gmail.com>
2019-06-18 08:39:43 +0900

    feat: cssを追加

--- nokogiri_test/main.rb
+++ nokogiri_test/main.rb
@@ -8,6 +8,13 @@ html = open('./index.html', &:read)
 doc = Nokogiri::HTML.parse(html, nil)
 doc.search('input').each do |element|
   puts "element.attributes: #{element.attributes}"
-  puts "is hidden?: #{element.attributes['type'].value == 'hidden'}"
+  puts "type: #{element.attributes['type'].value}"
+  puts "name: #{element.attributes['name'].value}" if element.attributes['name']
+  puts "id: #{element.attributes['id'].value}" if element.attributes['id']
   puts "value: #{element.attributes['value'].value}"
+end
+
+doc.css('.nokogiri').each do |element|
+  puts "element.attributes: #{element.attributes}"
+  puts "element.content: #{element.content}"
 end
