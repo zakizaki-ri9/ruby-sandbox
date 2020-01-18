568c3cf89272e51797c182a7fa42001ab0e31694
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-01-18 10:28:31 +0900

    feat: tdd-2

--- ruby_pro/04-tdd/rgb.rb
+++ ruby_pro/04-tdd/rgb.rb
@@ -2,8 +2,16 @@
 
 # rubocop:disable Naming/MethodParameterName
 def to_hex(r, g, b)
+  # injectでhexへ文字列を追記していく、数値だとhexに対して計算結果を継ぎ足していくイメージ
   [r, g, b].inject('#') do |hex, num|
+    # rjustで右パディング
     hex + num.to_s(16).rjust(2, '0')
   end
 end
 # rubocop:enable Naming/MethodParameterName
+
+def to_ints(rgb_str)
+  # scan - \w\wで単語構成文字:[a-zA-Z_0-9]が2文字連続で続く文字を取得
+  # ↑で取得した文字をhexで16進数→10進数変換した配列を作成
+  rgb_str.scan(/\w\w/).map(&:hex)
+end
