d6049d51168509402e087e9ad0cfe9af36c4ecd5
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-02-01 13:09:26 +0900

    monkey patch

--- ruby_pro/07_class/main.rb
+++ ruby_pro/07_class/main.rb
@@ -3,7 +3,7 @@
 # aliasのお試し
 class User
   def hello
-    'Hello!'
+    self.class.name
   end
 
   # alias 別名 元名 で定義可能
@@ -11,5 +11,20 @@ class User
 end
 
 u = User.new
-p u.hello
-p u.greeting
+p "User: #{u.hello}, #{u.greeting}"
+
+# モンキーパッチのお試し
+class User
+  # 元のhelloメソッドはhello_originalで別名定義
+  alias hello_original hello
+
+  # 再定義
+  # rubocop:disable Lint/DuplicateMethods
+  def hello
+    self.class.name + 'Ex'
+  end
+  # rubocop:enable Lint/DuplicateMethods
+end
+
+u = User.new
+p "User: #{u.hello}, #{u.greeting}, hello_original: #{u.hello_original}"
