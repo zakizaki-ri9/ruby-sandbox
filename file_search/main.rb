bf62fbce95523df50c83c4e0dffaa57b00acd69c
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-04-03 10:26:23 +0900

    file_search argv

--- file_search/main.rb
+++ file_search/main.rb
@@ -4,10 +4,19 @@ require 'csv'
 
 result = []
 
-File.open('./file_search/test.txt') do |f|
+# TODO: ログファイルパスを入力
+target_path = ARGV[0] || './file_search/test.txt'
+
+# TODO: 検索したいキーワードを入力
+search_keyword = ARGV[1] || 'Keyword'
+
+puts "ARGV: #{ARGV}, target_path: #{target_path}"
+
+File.open(target_path) do |f|
   f.each_line do |line|
-    str = line.scan(%r{^time:\d{4}\/\d{2}\/\d{2}\s\d{2}:\d{2}:\d{2}|Keyword.*$})
+    str = line.scan(%r{^time:\d{4}\/\d{2}\/\d{2}\s\d{2}:\d{2}:\d{2}|#{search_keyword}.*$})
     idx = result.find_index { |r| r[:key] == str[1] }
+    puts "str: #{str}, idx: #{idx}"
     if !idx.nil?
       result[idx][:count] += 1
     else
@@ -15,6 +24,8 @@ File.open('./file_search/test.txt') do |f|
     end
   end
 end
+
+puts "result: #{result}"
 
 result.sort! { |a, b| a[:count] <=> b[:count] }
 
